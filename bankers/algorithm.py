72d5f2099d9b1d199cae88ea03a49add26001dc2
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-05 12:49:29 +0530

    update

--- bankers/algorithm.py
+++ bankers/algorithm.py
@@ -1,1 +1,14 @@
 from typing import List, Tuple, Dict
+
+def compute_need(max_demand: List[List[int]],
+                 allocation: List[List[int]]) -> List[List[int]]:
+    """
+    need[i][j] = max_demand[i][j] - allocation[i][j]
+    """
+    n = len(max_demand)
+    m = len(max_demand[0]) if n > 0 else 0
+    need = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            need[i][j] = max_demand[i][j] - allocation[i][j]
+    return need
